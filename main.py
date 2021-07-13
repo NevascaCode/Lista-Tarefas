b8afa858de98e95f92b0936f2b96af82c6e6f38d
Kawan Henrique <kawan.inf@gmail.com>
2021-07-13 19:59:40 -0300

    NEW - adicionando tarefa na lista

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.card import MDCard
+from kivymd.uix.list import OneLineListItem
+Window.size = (360, 540)
 
-Window.size = (360, 640)
+class NovaTarefa(MDCard):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-kv ='''
-<MDBoxLayout>:
 
-'''
+class Tarefa(OneLineListItem):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class Tela(MDFloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 class ListaTarefas(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.click = True
     def build(self):
-        return Builder.load_file("lista_window.kv")
+        return Builder.load_file("lista_win.kv")
+
+    def adicionar_tarefa(self):
+        if self.click:
+            self.root.add_widget(NovaTarefa())
+
+    def criar_tarefa(self, text):
+        self.root.ids.lista_tarefas.add_widget(Tarefa(text=text))
 
 ListaTarefas().run()
